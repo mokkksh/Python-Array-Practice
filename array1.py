8755a7ef79f65ed5dd9d279a4b8ef366bde3feae
mokkksh <73828893+mokkksh@users.noreply.github.com>
2020-11-02 17:16:21 +0530

    Add files via upload

---
+++ array1.py
@@ -0,0 +1,15 @@
+arr = [10,20,30,40,50]
+print(arr)
+print(arr[0])
+print(arr[1])
+print(arr[2])
+print(arr[-1])
+print(arr[-2])
+
+brands=["apple","coke","microsoft","samsung","bentley"]
+print(brands)
+num_brands= len(brands)
+print(num_brands)
+brands.append("Intel")
+print(brands)
+
